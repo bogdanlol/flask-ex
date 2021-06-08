7211096b10374e43183b84b2f8dcd839a0a415bf
ys50wd <bogdan-marian.luca@ing.com>
2021-06-08 12:53:04 +0300

    chore(controller): rename controller file

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-from app.app_factory import create_app
+from app.controllers import create_app
 from app.settings import EnvConfiguration
 
 application = create_app(EnvConfiguration)
